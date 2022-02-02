d349636593cb30baa18ef7daf0826177d9f0b844
Dylan Greenberg <74738264+dylan-thegreenone@users.noreply.github.com>
2022-02-02 10:52:38 -0500

    Added documentation, mac and data functions

--- components/data/data.c
+++ components/data/data.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include "data.h"
 
-#define SEPARATOR "|"
-
-bluetooth_frame* new_frame()
+csha_bt_packet* new_frame()
 {
-    return malloc(sizeof(bluetooth_frame));
+    return malloc(sizeof(csha_bt_packet));
 }
 
-int calc_len(bluetooth_frame* frame)
+int calc_len(csha_bt_packet* frame)
 {
-    return strlen(frame -> name) + strlen(frame -> mac) + (frame -> rssi == 0 || frame -> rssi == 1 ? 1 : ceil(log10(frame -> rssi))) + 4;
+    return strlen(frame->name) + strlen(frame->mac) + (frame->rssi == 0 || frame->rssi == 1 ? 1 : ceil(log10(frame->rssi))) + 5 + 17;
 }
-
-char* format_data(char* str, bluetooth_frame* frame)
+char* format_data(char* str, csha_bt_packet* frame)
 {
-    sprintf(str, "%s%s%s%s%d%s%s", frame -> name,SEPARATOR, frame -> mac,SEPARATOR, frame -> rssi,SEPARATOR,SEPARATOR);
+    sprintf(str, "%s%s%s%s%d%s%s", frame->name,SEPARATOR, frame->mac,SEPARATOR, frame->rssi,SEPARATOR,SEPARATOR);
     return str;
 }
